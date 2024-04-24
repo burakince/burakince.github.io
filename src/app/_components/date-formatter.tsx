2ec34a89d8f232259a6c1303b52ca69dcd295464
Burak Ince <burak.ince@linux.org.tr>
2024-04-24 12:32:56 +0200

    Fix date parsing problem with default date
    Signed-off-by: Burak Ince <burak.ince@linux.org.tr>

--- src/app/_components/date-formatter.tsx
+++ src/app/_components/date-formatter.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const DateFormatter = ({ dateString }: Props) => {
-  const date = parseISO(dateString);
+  const date = parseISO(dateString || "1970-01-01T00:00:00.000Z");
   return (
     <time
       className="text-sm text-slate-500 dark:text-slate-400"
