6361e6eaa82920af64a6d92d05cedd8913b0e490
Burak Ince <burak.ince@linux.org.tr>
2024-04-26 10:40:59 +0200

    Change container to smaller version
    Signed-off-by: Burak Ince <burak.ince@linux.org.tr>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,19 @@ const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+
+      padding: "1rem",
+
+      screens: {
+        sm: "400px",
+        md: "486px",
+        lg: "656px",
+        xl: "827px",
+        "2xl": "998px",
+      },
+    },
   },
   plugins: [typography],
 };
