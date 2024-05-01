51bf230f339ae9490df98e98c2f17cbb3ebfe130
Burak Ince <burak.ince@linux.org.tr>
2024-05-01 15:35:54 +0200

    Fix sitemap and robots.txt files configuration
    Signed-off-by: Burak Ince <burak.ince@linux.org.tr>

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,21 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.burakince.com",
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/*.json$",
+          "/*_buildManifest.js$",
+          "/*_middlewareManifest.js$",
+          "/*_ssgManifest.js$",
+          "/*.js$",
+        ],
+      },
+    ],
+  },
+  trailingSlash: true,
   output: "export",
 };
