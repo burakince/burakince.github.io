e4d14cc398634a7a4964127371bde1e1a7048840
Burak Ince <burak.ince@linux.org.tr>
2025-01-24 09:19:35 +0100

    Fix missing migrations for tailwindcss v4

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const PostPreview = ({ title, date, excerpt, slug }: Props) => {
   return (
-    <div className="border border-slate-300 p-4 rounded-md shadow-sm bg-white dark:bg-slate-900">
+    <div className="border border-slate-300 p-4 rounded-md shadow-xs bg-white dark:bg-slate-900">
       <DateFormatter dateString={date} />
       <Link as={`/post/${slug}`} href="/post/[slug]">
         <h2 className="text-violet-600 dark:text-violet-500 hover:underline mb-4">
